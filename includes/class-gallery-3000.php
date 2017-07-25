209de1432e61a9a0bee9ce5ba9b4acd4ef5a3561
Barry Ceelen <b@rryceelen.com>
2017-07-25 04:30:46 +0200

    Improve wpcs compliance

--- includes/class-gallery-3000.php
+++ includes/class-gallery-3000.php
@@ -57,7 +57,7 @@ class Gallery_3000 {
 	public static function get_instance() {
 		// If the single instance hasn't been set, set it now.
 		if ( null === self::$instance ) {
-			self::$instance = new self;
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
